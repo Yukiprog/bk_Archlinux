0829772b73fa1b465449707ad52e5eafea933da7
Yukiprog <nomad048@gmail.com>
2021-09-04 00:55:06 +0900

    exclude file and dir

--- .emacs.d/elpa/ac-php-20200916.751/ac-php-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "ac-php" "20200916.751" "Auto Completion source for PHP." '((ac-php-core "2.0") (auto-complete "1.4.0") (yasnippet "0.8.0")) :commit "9770c95bf2df93d9cb0f200723b03b3d9a480640" :authors '(("jim" . "xcwenn@qq.com")) :maintainer '("jim") :keywords '("completion" "convenience" "intellisense") :url "https://github.com/xcwen/ac-php")
--- .emacs.d/elpa/mozc-20210306.1053/mozc-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "mozc" "20210306.1053" "minor mode to input Japanese with Mozc" 'nil :commit "884d9f4b64fe97597643247f1bed1d4fc9ca5bbf" :keywords '("mule" "multilingual" "input method"))
